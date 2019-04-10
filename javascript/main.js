aa5966ed04614b56b588bcf7434e10ce66a6921d
mads5856 <mads.normann@jubii.dk>
2019-04-10 11:34:55 +0200

    Javascript til pige indstillinger

--- javascript/main.js
+++ javascript/main.js
@@ -36,6 +36,8 @@ document.getElementById("btnPrev").addEventListener("click", function(){
     document.getElementById("sliderImage").src = imageList[currentIndex];
 })
 
+//Haarfarve af dreng:
+
 document.getElementById("btnHair").addEventListener("click", function(){
     
     if(currentIndex == 0){
@@ -52,4 +54,41 @@ document.getElementById("btnHair").addEventListener("click", function(){
     
 })
 
+
+
 //Haarstil af pige:
+
+let imageListPige = [];
+imageListPige[0] = "../imgs/pige/avatar-pige.png";
+imageListPige[1] = "../imgs/pige/avatar-pige-2.png";
+imageListPige[2] = "../imgs/pige/avatar-pige-3.png";
+imageListPige[3] = "../imgs/pige/avatar-pige-4.png";
+imageListPige[4] = "../imgs/pige/avatar-pige-5.png";
+
+let currentIndexGirl = 0;
+let currentImageNumberGirl = currentIndex + 1;
+
+
+document.getElementById("btnNextGirl").addEventListener("click", function(){
+    currentIndex++;
+    
+    if(currentIndexGirl >= imageListPige.length){
+        currentIndex = 0;
+    }
+    
+    let currentImageNumberGirl = currentIndexGirl + 1;
+    
+    document.getElementById("sliderImageGirl").src = imageListPige[currentIndexGirl];    
+})
+
+document.getElementById("btnPrevGirl").addEventListener("click", function(){
+    currentIndexGirl--;
+    
+    if(currentIndexGirl <= - 1){
+        currentIndexGirl = imageListPige.length - 1;
+    }
+    
+    let currentImageNumberGirl = currentIndexGirl + 1;
+    
+    document.getElementById("sliderImageGirl").src = imageListPige[currentIndexGirl];
+})
